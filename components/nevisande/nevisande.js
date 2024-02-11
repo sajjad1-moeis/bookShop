630fad6952aa69e81109f995bd70763a42921721
sajjad1-moeis <154022929+sajjad1-moeis@users.noreply.github.com>
2024-02-11 12:17:19 +0330

    f

--- components/nevisande/nevisande.js
+++ components/nevisande/nevisande.js
@@ -5,8 +5,8 @@ template.innerHTML = `
 <div>
 <img src="./img/n1.jpg" alt="" class="rounded-full persent" />
 <div class="flex gap-2 w-max mx-auto mt-[-35px]">
-  <div class="p-4 bg-white rounded-full shadow-lg" style="z-index:555" ><img src="./img/icons8-facebook-50.png" class="w-8" alt="" /></div>
-  <div class="p-4 bg-white rounded-full shadow-lg" style="z-index:555" ><img src="./img/icons8-facebook-50.png" class="w-8" alt="" /></div>
+  <div class="p-2 md:p-4 bg-white rounded-full shadow-lg" style="z-index:555" ><img src="./img/icons8-facebook-50.png" class="w-5 md:w-8" alt="" /></div>
+  <div class="p-2 md:p-4 bg-white rounded-full shadow-lg" style="z-index:555" ><img src="./img/icons8-facebook-50.png" class="w-5 md:w-8" alt="" /></div>
 </div>
 <div class="my-6 text-center">
   <h3 class="title1 text-3xl my-2.5">مایک براون</h3>
