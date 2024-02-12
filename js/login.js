2b6c9d24a6cfd7b1a8c057bb91e5215b1ed10298
sajjad1-moeis <154022929+sajjad1-moeis@users.noreply.github.com>
2024-02-12 18:36:21 +0330

    fixed

--- js/login.js
+++ js/login.js
@@ -1,92 +1,4 @@
-// let $ = document;
-// let flag = true;
-// let SaveUser = $.querySelector(".SaveUser");
-// SaveUser.addEventListener("click", () => {
-//   if (flag) {
-//     flag = false;
-//     SaveUser.classList.replace("bg-black", "bg-blue-700");
-//     SaveUser.style.textAlign = "-webkit-left";
-//   } else {
-//     flag = true;
-//     SaveUser.style.textAlign = "-webkit-right";
-//     SaveUser.classList.replace("bg-blue-700", "bg-black");
-//   }
-// });
-
-// let cokie = $.cookie;
-// let loginText = $.querySelectorAll(".loginText");
-// let btnSubmit = $.querySelector(".btnSubmit");
-// let btnLogin = $.querySelector(".btnLogin");
-// let inputLogin = $.querySelectorAll(".inputLogin");
-// let inputSubmit = $.querySelectorAll(".inputSubmit");
-// loginText.forEach((btn) => {
-//   btn.onclick = () => {
-//     if (btn.className.includes("Submit")) {
-//       $.querySelector(".DivSubmit").classList.remove("hidden");
-//       $.querySelector(".DivLogin").classList.add("hidden");
-//     } else {
-//       $.querySelector(".DivSubmit").classList.add("hidden");
-//       $.querySelector(".DivLogin").classList.remove("hidden");
-//     }
-//     loginText.forEach((btn) => btn.classList.remove("active"));
-//     btn.classList.add("active");
-//   };
-// });
-// btnLogin.addEventListener("click", () => {
-//   if (inputLogin[0].value && inputLogin[1].value) {
-//     if (cokie) {
-//       let cokie = document.cookie.split(";");
-//       let y = cokie.map((item) => item.slice(item.indexOf("=") + 1));
-//       console.log(y);
-//       if (inputLogin[0].value === `${y[0]}${y[1]}` && inputLogin[1].value === y[2]) {
-//         alert("با موفقیت ورود کردید");
-//         location.href = "../index.html";
-//       } else {
-//         $.querySelector(".ElmNotLogin").innerHTML = "نام کاربری یا پسوورد اشتباه است";
-//       }
-//     } else {
-//       $.querySelector(".ElmNotLogin").innerHTML = "ابتدا ثبت نام کنید";
-//     }
-//   } else {
-//     $.querySelector(".ElmNotLogin").innerHTML = "لطفا مقادیر را به درستی وارد کنید";
-//   }
-// });
-// btnSubmit.addEventListener("click", () => {
-//   if (cokie) {
-//     $.querySelector(".ElmNotSubmit").innerHTML = "وارد حساب خود شوید";
-//   } else {
-//     if (inputSubmit[2].value != inputSubmit[3].value) {
-//       $.querySelector(".ElmNotSubmit").innerHTML = "لطفا پسوورد رو عین هم تکرار کنید";
-//     } else if (inputSubmit[0].value && inputSubmit[1].value && inputSubmit[2].value && inputSubmit[3].value) {
-//       let date = new Date();
-//       console.log(date.getTime());
-//       let now = date.setTime(date.getTime() + 20 * 24 * 60 * 60 * 1000);
-//       $.cookie = `name=${inputSubmit[0].value};path=/;expires=${date}`;
-//       $.cookie = `family=${inputSubmit[1].value};path=/;expires=${date}`;
-//       $.cookie = `pass=${inputSubmit[3].value};path=/;expires=${date}`;
-//       alert("ثبت نام با موفقیت انجام شد");
-//       setTimeout(() => {
-//         location.reload();
-//       }, 500);
-//     }
-//   }
-// });
-
-// ///ShowPass
-
-// $.querySelectorAll(".ShowPass").forEach((ShowPassBtn) => {
-//   ShowPassBtn.addEventListener("click", (e) => {
-//     let parent = e.target.parentElement;
-//     let input = parent.querySelector("input");
-//     if (e.target.src === "http://127.0.0.1:5501/img/eye.png") {
-//       ShowPassBtn.src = "../img/eye-slash.png";
-//       input.type = "text";
-//     } else {
-//       input.type = "password";
-//       ShowPassBtn.src = "../img/eye.png";
-//     }
-//   });
-// });
+let $ = document;
 
 const inputs = document.querySelectorAll(".input");
 
@@ -130,14 +42,19 @@ const showPassBtn = document.querySelectorAll(".showPassBtn");
 const removeClass = (add) => {
   spanError[add].classList.remove("hidden");
 };
-
+console.log("d@gmail.com".length);
 ///////////Function Regester
+
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
 
 const regesterFunc = () => {
   spanError.forEach((span) => span.classList.add("hidden"));
   if (inputDivRegestar[0].value.length < 8) {
     removeClass(0);
-  } else if (inputDivRegestar[1].value.length < 4) {
+  } else if (!validateEmail(inputDivRegestar[1].value)) {
     removeClass(1);
   } else if (inputDivRegestar[2].value.length < 8) {
     removeClass(2);
@@ -172,3 +89,6 @@ showPassBtn.forEach((btn) => {
 });
 registerBtn.onclick = regesterFunc;
 loginBtn.onclick = loginFunc;
+
+const email = "example@gmail.com";
+console.log(validateEmail(email));
